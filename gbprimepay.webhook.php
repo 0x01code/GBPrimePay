b38249061fdb04ac58f7d6ce7c387e95d122b663
0x01code <donghappy123@gmail.com>
2023-01-15 20:35:29 +0700

    first commit

---
+++ gbprimepay.webhook.php
@@ -0,0 +1,10 @@
+<?php
+
+$respFile = fopen("resp-log.txt", "w") or die("Unable to open file!");
+
+$json_str = file_get_contents('php://input');
+fwrite($respFile, $json_str . "\n\n");
+
+$json_obj = json_decode($json_str);
+fwrite($respFile, $json_obj);
+fclose($respFile);
