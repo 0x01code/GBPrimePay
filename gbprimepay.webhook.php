2a80762ffaf3ca8e1cde234c481e30169b444801
0x01code <donghappy123@gmail.com>
2023-01-16 21:33:22 +0700

    Update Webhook

--- gbprimepay.webhook.php
+++ gbprimepay.webhook.php
@@ -1,10 +1,19 @@
 <?php
 
-$respFile = fopen("resp-log.json", "w") or die("Unable to open file!");
+// ถ้าเป็น ip ของ GBPrimePay ถึงจะให้ทำงานโค้ดใน if
+if ($_SERVER['REMOTE_ADDR'] == '203.151.205.45' || $_SERVER['REMOTE_ADDR'] == '203.151.205.33') {
+    $respFile = fopen("resp-log.json", "w") or die("Unable to open file!");
 
-$json_str = file_get_contents('php://input');
-fwrite($respFile, $json_str . "\n\n");
+    $json_str = file_get_contents('php://input');
+    fwrite($respFile, $json_str . "\n\n");
 
-$json_obj = json_decode($json_str);
-fwrite($respFile, $json_obj);
-fclose($respFile);
+    $json_obj = json_decode($json_str);
+
+    // $result = $db->query("SELECT user_id FROM topup WHERE refcode = '{$json_obj->referenceNo}'");
+    // $db->query("UPDATE users SET point = point + {$json_obj->amount} WHERE id = {$result->user_id}");
+
+    fwrite($respFile, $json_obj);
+    fclose($respFile);
+} else {
+    die('Access Denied');
+}
